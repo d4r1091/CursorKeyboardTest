439952c21be6c4df9cadd7879b78d54d43affb13
Dario Carlomagno <carlomagno@webmonks.it>
2014-11-18 14:19:34 +0100

    primi test per funzionamento textfield

--- CursorKeyboardTest/ViewController.h
+++ CursorKeyboardTest/ViewController.h
@@ -8,7 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate, UIGestureRecognizerDelegate>
+
+
+@property (weak, nonatomic) IBOutlet UITextField *myTextField;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeLeft;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeRight;
+
+- (IBAction)prevButtonPressed:(id)sender;
+- (IBAction)nextButtonPressed:(id)sender;
 
 
 @end
--- CursorKeyboardTest/ViewController.m
+++ CursorKeyboardTest/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UITextPosition *currentCursorPosition;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +19,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [_myTextField becomeFirstResponder];
+    
+    _swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    _swipeLeft.delegate = self;
+    [self.view addGestureRecognizer:_swipeLeft];
+    
+    _swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    _swipeRight.delegate = self;
+    [self.view addGestureRecognizer:_swipeRight];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +36,34 @@
     // Dispose of any resources that can be recreated.
 }
 
-@end
+- (IBAction)prevButtonPressed:(id)sender {
+    currentCursorPosition = [_myTextField positionFromPosition:currentCursorPosition inDirection:UITextLayoutDirectionLeft offset:1];
+    [_myTextField setSelectedTextRange:[_myTextField textRangeFromPosition:currentCursorPosition toPosition:currentCursorPosition]];
+}
+
+- (IBAction)nextButtonPressed:(id)sender {
+    currentCursorPosition = [_myTextField positionFromPosition:currentCursorPosition inDirection:UITextLayoutDirectionRight offset:1];
+    [_myTextField setSelectedTextRange:[_myTextField textRangeFromPosition:currentCursorPosition toPosition:currentCursorPosition]];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    UITextPosition *beginning = [textField beginningOfDocument];
+    [textField setSelectedTextRange:[textField textRangeFromPosition:beginning
+                                                          toPosition:beginning]];
+}
+
+
+- (void)handleSwipe:(UISwipeGestureRecognizer *)swipe {
+    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionLeft) {
+        NSLog(@"Left Swipe");
+        [self prevButtonPressed:nil];
+    }
+    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionRight) {
+        NSLog(@"Right Swipe");
+        [self nextButtonPressed:nil];
+    }
+    
+}@end
